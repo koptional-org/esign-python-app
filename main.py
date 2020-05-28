874ea4765ec1a4e68d2b2e2463a74e16f2431fa1
Name <emmaascolese@gmail.com>
2020-05-28 16:43:31 +0000

    signup forms added and routes added

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
