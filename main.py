592f686bd95769e17cddeda9ed7dfae383454868
John P Considine <jconsidi@nd.edu>
2020-05-28 17:01:45 +0000

    feat: add home page user interface

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
 
 
 @app.route('/login')
@@ -21,4 +16,14 @@ def signup():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return redirect("/home/active")
+
+
+@app.route('/home/active')
+def home_active():
+    return render_template('home.html', type="active")
+
+
+@app.route('/home/completed')
+def home_completed():
+    return render_template('home.html', type="completed")
