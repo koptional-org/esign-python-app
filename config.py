b7566a8f8bee63a81da179e2a7f25bf52a5a3857
John P Considine <jconsidi@nd.edu>
2020-06-26 20:08:13 +0000

    chore: boilerplate for config

--- config.py
+++ config.py
@@ -5,3 +5,10 @@ DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
+
+WS_API_KEY = ""  # get from dashboard
+
+WS_CONTRACT_ID = ''  # get from dashboard
+DEV_EMAIL = ""  # your email
+DEV_FIRST_NAME = ""  # your name
+DEV_LAST_NAME = ""  # your last name
