e1fdb8ef71b3c7bf778ab008d70da398fa278f7f
John P Considine <jconsidi@nd.edu>
2021-02-17 02:11:41 +0000

    update config

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 
-WS_API_KEY = ""  # get from dashboard
+WS_API_KEY = "AK7OjD2YlM38X6f6vfZZvTaQ6LkzTAxutRN3VWYpEM"  # get from dashboard
 
-WS_CONTRACT_ID = ''  # get from dashboard
-DEV_EMAIL = ""  # your email
-DEV_FIRST_NAME = ""  # your name
-DEV_LAST_NAME = ""  # your last name
+WS_CONTRACT_ID = '9AVz5e5Bbw6OyB'  # get from dashboard
+DEV_EMAIL = "jackconsidine3@gmail.com"  # your email
+DEV_FIRST_NAME = "John"  # your name
+DEV_LAST_NAME = "Considine"  # your last name
